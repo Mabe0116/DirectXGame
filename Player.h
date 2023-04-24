103e7fe0e091853d981b8a92935478df84fafdee
阿部光樹 <@MSI>
2023-04-24 13:33:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#pragma once
+class Player {
+public:
+	void Initialize(Model* model, uint32_t textureHandle);
+	void Update();
+	void Draw(ViewProjection viewProjection);
+
+private:
+	//ワールド座標データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
---
+++ scene/GameScene.cpp
@@ -0,0 +1,93 @@
+#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() { 
+	delete sprite_;
+	delete model_;
+	delete player_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	textureHandle_ = TextureManager::Load("debugfont.png");
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("debugfont.png");
+	//3Dモデルの生成
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	player_ = new Player();
+	player_->Initialize(model_,textureHandle_);
+
+}
+
+void GameScene::Update() {
+	//スプライトの今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	//座標を{2,1}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	player_->Update();
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+	/*sprite_->Draw();*/
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	player_->Draw(viewProjection_);
+
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
